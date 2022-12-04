8a7717d2549668d31cbc3cd0fe633fc90c60dd32
Shunyi Xu <20100201@mail.wit.ie>
2022-12-04 14:35:40 +0000

    Mongo lab Error Handling

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,21 +1,12 @@
 import express from 'express';
 import User from './userModel';
-
+import asyncHandler from 'express-async-handler';
 const router = express.Router(); // eslint-disable-line
 
 // Get all users
 router.get('/', async (req, res) => {
     const users = await User.find();
     res.status(200).json(users);
-});
-
-// register(Create)/Authenticate User
-router.post('/', async (req, res) => {
-    await User(req.body).save();
-    res.status(201).json({
-        code: 201,
-        msg: 'Successful created new user.',
-    });
 });
 
 router.put('/:id', async (req, res) => {
@@ -29,7 +20,6 @@ router.put('/:id', async (req, res) => {
         res.status(404).json({ code: 404, msg: 'Unable to Update User' });
     }
 });
-
 router.post('/:id/favourites', async (req, res) => {
     const newFavourite = req.body;
     if (newFavourite && newFavourite.id) {
@@ -43,26 +33,6 @@ router.post('/:id/favourites', async (req, res) => {
         }
     }
 });
-
-router.post('/', async (req, res) => {
-    if (req.query.action === 'register') {  //if action is 'register' then save to DB
-        await User(req.body).save();
-        res.status(201).json({
-            code: 201,
-            msg: 'Successful created new user.',
-        });
-    }
-    else {  //Must be an authenticate then!!! Query the DB and check if there's a match
-        const user = await User.findOne(req.body);
-        if (!user) {
-            return res.status(401).json({ code: 401, msg: 'Authentication failed' });
-        }else{
-            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
-        }
-    }
-});
-
-
 router.get('/:id/favourites', async (req, res) => {
     const user = await User.findById(req.params.id);
     if (user) {
@@ -71,5 +41,23 @@ router.get('/:id/favourites', async (req, res) => {
         res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
     }
 });
+
+router.post('/', asyncHandler(async (req, res) => {
+    if (req.query.action === 'register') {  //if action is 'register' then save to DB
+        await User(req.body).save()
+        res.status(201).json({
+            code: 201,
+            msg: 'Successful created new user.',
+        });
+    }
+    else {  //Must be authenticating then! Query the DB and check if there's a match
+        const user = await User.findOne(req.body);
+        if (!user) {
+            return res.status(401).json({ code: 401, msg: 'Authentication failed' })
+        } else {
+            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' })
+        }
+    }
+}));
 
 export default router;
