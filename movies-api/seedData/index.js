5906e24e8e176a12d0be9bfbfb4ec635cc9c28d4
Shunyi Xu <20100201@mail.wit.ie>
2022-12-07 04:16:45 +0000

    Topic 11 - Update Movies Data/Route

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,10 +1,24 @@
 import userModel from '../api/users/userModel';
 import genresModel from '../api/genres/genresModel';
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js';
 import users from './users';
 import genres from './genres';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+export async function loadMovies() {
+  console.log('load seed data');
+  console.log(movies.length);
+  try {
+    await movieModel.deleteMany();
+    await movieModel.collection.insertMany(movies);
+    console.info(`${movies.length} Movies were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load movie Data: ${err}`);
+  }
+}
 
 // deletes all user documents in collection and inserts test data
 async function loadUsers() {
@@ -32,4 +46,5 @@ async function loadGenres() {
 if (process.env.SEED_DB) {
   loadUsers();
   loadGenres();
+  loadMovies();
 }
