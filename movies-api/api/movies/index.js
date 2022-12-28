827ecedb11794d69143bfd6c2a1a0fc21f13f0f0
Shunyi Xu <20100201@mail.wit.ie>
2022-12-28 23:15:23 +0000

    Topic 12 - Finished!

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,6 +3,7 @@ import { movieReviews} from './moviesData';
 import uniqid from 'uniqid';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
+import { getUpcomingMovies } from '../tmdb-api';
 
 const router = express.Router(); 
 router.get('/:id/reviews', (req, res) => {
@@ -49,5 +50,10 @@ router.post('/:id/reviews', (req, res) => {
         });
     }
 });
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+}));
+
 
 export default router;
